fa57139c455c9f46c5db3a7a201efad51d40ba79
wmsi <wchurch@whitemountainscience.org>
2016-06-19 11:13:05 -0400

    edit 1
    changed html/python code to drive motor when arrow is clicked.

--- control_motor.py
+++ control_motor.py
@@ -10,6 +10,6 @@ runTime = 3000
 
 #function called by javascript to set state
 def setState(state):
-    power = int(state)*75
-        m.run_timed(time_sp=runTime,duty_cycle_sp=power)
-        print(state)
+	power = int(state)*75
+	m.run_timed(time_sp=runTime,duty_cycle_sp=power)
+	print(state)
